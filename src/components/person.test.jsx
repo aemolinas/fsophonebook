b921e1309aba9789f852123213eb94a2b05b0f01
Amar Molinas <aemolinas@gmail.com>
2023-08-02 21:11:01 -0500

    feature:person.test.jsx now tests and and number

--- src/components/person.test.jsx
+++ src/components/person.test.jsx
@@ -4,8 +4,9 @@ import { render, screen } from "@testing-library/react"
 
 test("displays name based on props", () => {
     const name="bob"
-    const number='12345-678910-1112'
+    const number="12345-678910-1112"
     render(<Person name={name} number={number} />)
 
-    expect(screen.getByText(name)).toBeTruthy()
+    expect(screen.getByText(`${name}: ${number}`)).toBeTruthy()
+    
 })
