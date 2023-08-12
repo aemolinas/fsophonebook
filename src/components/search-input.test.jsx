0775cb1632d9f5050be8a675062fba954bf8ae15
Amar Molinas <aemolinas@gmail.com>
2023-08-11 20:25:49 -0500

    feature: added test for search input

--- src/components/search-input.test.jsx
+++ src/components/search-input.test.jsx
@@ -1,11 +1,13 @@
 import { test, vi, expect } from "vitest"
-import {render, screen, act } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import NameFilter from "./SearchInput"
+import SearchInput from "./SearchInput"
 
-test('generates a search filter key from user input and passes it to a form', () => {
-  //the name filter should be a an input field outside the contact submission form
-  //it should dynamically accept user input and update the value of a useState hook called "???"
-  //the filtered value should be used in an array.filter method on persons to produce a filtered list of persons
-  //the filtered list should be what is passed to NameOutput
-})
+test('calls change handler with a new value', async () => {
+  const changeHandler = vi.fn()
+  render(<SearchInput value='' handSubmit={changeHandler} />)
+  const testSearchKey = 'Arto Hellas'
+  const inputValue = screen.getByLabelText('')
+  await userEvent.type(inputValue, testSearchKey)
+  expect(changeHandler).toHaveBeenNthCalledWith(testSearchKey)  
+} ) 
